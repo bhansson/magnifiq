aa30831fbbeed4929bdd30e0068ec6f7dc6e7f4f
bhansson <bhansson@gmail.com>
2025-11-16 13:20:59 +0100

    feat: configure customizable server setup and enhance authentication logo styling
    - Add host, port, and HMR configuration to Vite server setup
    - Update docker-compose to fix port mapping
    - Improve authentication logo styling for better responsiveness and scalability

--- resources/views/components/authentication-card-logo.blade.php
+++ resources/views/components/authentication-card-logo.blade.php
@@ -1,6 +1,6 @@
 <a href="/">
     @if(isset($partnerTeam) && $partnerTeam && $partnerTeam->logo_path)
-        <img src="{{ asset('storage/' . $partnerTeam->logo_path) }}" alt="{{ $partnerTeam->name }}" class="size-16 object-contain" />
+        <img src="{{ asset('storage/' . $partnerTeam->logo_path) }}" alt="{{ $partnerTeam->name }}" class="h-32 w-auto max-w-xs object-contain" />
     @else
         <svg class="size-16" viewbox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M11.395 44.428C4.557 40.198 0 32.632 0 24 0 10.745 10.745 0 24 0a23.891 23.891 0 0113.997 4.502c-.2 17.907-11.097 33.245-26.602 39.926z" fill="#6875F5"/>
